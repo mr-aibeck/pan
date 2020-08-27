b341c32bf7e4a79d749a32dade36bcf139c47551
Aibeck <mr.aibeck@gmail.com>
2020-08-27 16:21:44 +0300

    Updates

---
+++ precache-manifest.dcc1a3cd5dfd5c5df294060d775b44bc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba4d5cc58dcbd85589260e049ae0455",
+    "url": "/pan/index.html"
+  },
+  {
+    "revision": "61b0fb2240cb43c81eb7",
+    "url": "/pan/static/css/main.1b23a45e.chunk.css"
+  },
+  {
+    "revision": "1d3a6162f711e73c534c",
+    "url": "/pan/static/js/2.13df7cea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pan/static/js/2.13df7cea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b0fb2240cb43c81eb7",
+    "url": "/pan/static/js/main.01f0e93d.chunk.js"
+  },
+  {
+    "revision": "24fa958bcf2c9c002f15",
+    "url": "/pan/static/js/runtime-main.edf5a585.js"
+  },
+  {
+    "revision": "cb84faa4f473f88fbb5acf39cc1261c7",
+    "url": "/pan/static/media/Formular-Bold.cb84faa4.woff2"
+  },
+  {
+    "revision": "d156fb5cdb81c0e2c303d5693b6c8daf",
+    "url": "/pan/static/media/Formular-Bold.d156fb5c.woff"
+  },
+  {
+    "revision": "86134a9473dc135a4fb221eb26038362",
+    "url": "/pan/static/media/Formular.86134a94.woff2"
+  },
+  {
+    "revision": "b970ba7117a3e355d469e2eebf0bd23b",
+    "url": "/pan/static/media/Formular.b970ba71.woff"
+  },
+  {
+    "revision": "b8f2dee6eee7ac44ee7a8864aba3c8ab",
+    "url": "/pan/static/media/welcome.b8f2dee6.png"
+  }
+]);
